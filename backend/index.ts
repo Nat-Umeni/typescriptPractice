0c51248c21d468ac484ecf33e8ee5cb7b6afefe6
Nathaniel Grey <itsnat381@gmail.com>
2025-04-28 21:15:28 +0100

    Properly implemented updating a task, need to do a proper error handling component now

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,15 @@
-import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
-import sqlite3 from "sqlite3";
-import { Task } from "./types";
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import sqlite3 from 'sqlite3';
+import { Task } from './types';
 const app = express();
 
-const db = new sqlite3.Database("./database.db");
+const db = new sqlite3.Database('./database.db');
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
+    cors({
+        origin: 'http://localhost:5173'
+    })
 );
 
 app.use(express.json());
@@ -17,72 +17,75 @@ app.use(express.json());
 const port = process.env.PORT || 1507;
 
 db.serialize(() => {
-  db.run("DROP TABLE IF EXISTS tasks");
+    db.run('DROP TABLE IF EXISTS tasks');
 
-  db.run(
-    "CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN)"
-  );
+    db.run('CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN)');
 });
 
-
-app.get('/', (_req, res) => {
-  res.send('OK');
+app.get('/', (req: Request, res: Response) => {
+    res.send('OK');
 });
 
-app.get("/tasks", (req: Request, res: Response) => {
-  db.all<Task[]>("SELECT * FROM tasks", (err, rows) => {
-    if (err) {
-      console.error(err.message);
-      return res.status(500).json({ error: err.message });
-    }
+app.get('/tasks', (req: Request, res: Response) => {
+    db.all<Task[]>('SELECT * FROM tasks', (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ error: err.message });
+        }
 
-    res.json(rows);
-  });
+        res.json(rows);
+    });
 });
 
-app.post("/tasks", (req: Request, res: Response) => {
-  const task: Task = req.body;
-  db.run(
-    "INSERT INTO tasks (title, completed) VALUES (?, ?)",
-    [task.title, task.completed],
-    function (err) {
-      if (err) {
-        console.error(err.message);
-        return res.status(500).json({ error: err.message });
-      }
+app.post('/tasks', (req: Request, res: Response) => {
+    const task: Task = req.body;
+    db.run(
+        'INSERT INTO tasks (title, completed) VALUES (?, ?)',
+        [task.title, task.completed],
+        function (err: Error | null) {
+            if (err) {
+                console.error(err.message);
+                return res.status(500).json({ error: err.message });
+            }
 
-      console.log("Created task!");
+            console.log('Created task!');
 
-      const insertedTask: Task = {
-        id: this.lastID,
-        title: task.title,
-        completed: task.completed,
-      };
+            const insertedTask: Task = {
+                id: this.lastID,
+                title: task.title,
+                completed: task.completed
+            };
 
-      console.log(insertedTask);
+            console.log(insertedTask);
 
-      res.status(201).json({
-        task: insertedTask,
-        message: "Task created successfully",
-      });
-    }
-  );
+            res.status(201).json({
+                task: insertedTask,
+                message: 'Task created successfully'
+            });
+        }
+    );
 });
 
 app.put('/tasks/:id', (req: Request, res: Response, next: NextFunction) => {
-  console.log("PUT /tasks/:id called with id:", req.params.id);
+    const { title, completed, id }: Task = req.body.task;
 
-  // db.run(
-  //   "UPDATE tasks SET title = ?, completed = ? WHERE id = ?",
-  //   [req.body.title, req.body.completed, req.params.id],
-  //   function (err) {
-  //     if (err) {
-  //       console.error(err.message);
-  //       return res.status(500).json({ error: err.message });
-  //     }
-  // });
+    if (title.trim() === '' || completed || !id) {
+        console.error('Missing required fields');
+        res.status(400).json({ error: 'Missing required fields' });
+        return;
+    }
 
-  res.status(200).json({ message: "Route hit" });
+    db.run('UPDATE tasks SET title = ?, completed = ? WHERE id = ?', [title, completed, id], function (err) {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ error: err.message });
+        }
+    });
+
+    console.log('Updated the task');
+
+    res.status(200).json({ message: 'Updated the task', task: req.body.task });
+    return;
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,196 +1,196 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import "./App.css";
-import { Task } from "./types";
-import { IoMdAddCircleOutline, IoMdSave } from "react-icons/io";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import { Task } from './types';
+import { IoMdAddCircleOutline, IoMdSave } from 'react-icons/io';
 import {
-  // AiOutlineFileDone,
-  AiOutlineDelete,
-  AiOutlineEdit,
-} from "react-icons/ai";
+    // AiOutlineFileDone,
+    AiOutlineDelete,
+    AiOutlineEdit
+} from 'react-icons/ai';
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [title, setTitle] = useState<string>("");
-  const [editedTitle, setEditedTitle] = useState<string>("");
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [title, setTitle] = useState<string>('');
+    const [editedTitle, setEditedTitle] = useState<string>('');
 
-  const addTask = async (title: string): Promise<void | boolean> => {
-    const response = await axios.post("/api/tasks", {
-        title: title,
-        completed: false,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const addTask = async (title: string): Promise<void | boolean> => {
 
-    if (response.status !== 201) {
-      console.error("Error adding task "+ response);
-      return false;
-    }
-
-    setTasks([...tasks, response.data.task]);
-
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    addTask(title);
-    setTitle("");
-  };
-
-  const [taskBeingEdited, setTaskBeingEdited] = useState<Task | null>(null);
-  const handleEditTask = (taskId: string|number): void => {
-    const task: Task | undefined = tasks.find((task) => task.id === taskId);
-    if (task) {
-      setTaskBeingEdited(task);
-      setEditedTitle(task.title);
-    }
-  };
-
-  const saveEditedTask = async () => {
-    if (!taskBeingEdited) return;
-
-    const task: Task | undefined = tasks.find((task) => task.id === taskBeingEdited.id);
-
-    if (task) {
-      task.title = editedTitle;
-
-      try {
-        const response = await axios.put(`/api/tasks/${taskBeingEdited.id}`, {
-          task: task
-        });
-
-        // console.log(response)
-
-        if (response.status !== 200) {
-          throw new Error("Error updating task " + response)
-        }
-
-      } catch (error) {
-        console.error(error);
-      }
-   
-      setTaskBeingEdited(null);
-      setEditedTitle("");
-
-      // Update the tasks state
-
-    }
-  };
-
-  const handleTaskDelete = (taskId: string|number): void => {
-    setTasks((prevTasks: Task[]): Task[] => {
-      return prevTasks.filter((task) => task.id !== taskId);
-    });
-  };
-
-  // On page load get all the tasks and add them to state
-  useEffect(() => {
-    axios.get("/api/tasks").then(({ data }) =>{
-      setTasks(data)
-    });
-  }, []);
-
-  // For debugging
-  useEffect(() => { 
-    console.log(tasks);
-  }, [tasks]);
-
-  return (
-    <>
-      <h1 className="mb-4 text-3xl font-bold text-center">Add a task</h1>
-
-      <div className="p-8">
-        <form onSubmit={handleSubmit} className="flex gap-2">
-          <input
-            type="text"
-            className="flex-1 px-4 py-2 rounded bg-[#1a1a1a] focus:outline-none focus:ring-2 focus:ring-[#646cff] transition-all duration-200"
-            placeholder="Task title"
-            onInput={(e) => setTitle(e.currentTarget.value)}
-            value={title}
-          />
-          <button
-            type="submit"
-            aria-label="Add task"
-            className="text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
-          >
-            <IoMdAddCircleOutline title="Add task" />
-          </button>
-        </form>
-      </div>
-
-      <ul className="mt-4 space-y-2">
-        {tasks.map((task) => (
-          <div
-            key={task.id}
-            className="flex justify-between items-center p-4 rounded border border-[#646cff]"
-          >
-            <div>
-              {taskBeingEdited?.id === task.id ? (
-                <input
-                  type="text"
-                  className="flex-1 px-4 py-2 rounded bg-[#1a1a1a] focus:outline-none focus:ring-2 focus:ring-[#646cff] transition-all duration-200"
-                  placeholder="Task title"
-                  onInput={(e) => setEditedTitle(e.currentTarget.value)}
-                  onBlur={saveEditedTask}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      saveEditedTask();
+        try {
+            const response = await axios.post(
+                '/api/tasks',
+                {
+                    title: title,
+                    completed: false
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
                     }
-                  }}
-                  value={editedTitle}
-                />
-              ) : (
-                <span className="text-white">{task.title}</span>
-              )}
+                }
+            );
+    
+            setTasks([...tasks, response.data.task]);
+            
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        addTask(title);
+        setTitle('');
+    };
+
+    const [taskBeingEdited, setTaskBeingEdited] = useState<Task | null>(null);
+    const handleEditTask = (taskId: string | number): void => {
+        const task: Task | undefined = tasks.find((task) => task.id === taskId);
+        if (task) {
+            setTaskBeingEdited(task);
+            setEditedTitle(task.title);
+        }
+    };
+
+    const saveEditedTask = async () => {
+        if (!taskBeingEdited) return;
+
+        const task: Task | undefined = tasks.find((task) => task.id === taskBeingEdited.id);
+
+        if (task) {
+            task.title = editedTitle;
+
+            try {
+                const response = await axios.put(`/api/tasks/${taskBeingEdited.id}`, {
+                    task
+                });
+
+                console.log(response.data.task);
+
+                // Update the tasks state
+                setTaskBeingEdited(null);
+                setEditedTitle('');
+                setTasks(tasks.map((t) => (t.id === task.id ? response.data.task : t)));
+
+            } catch (error) {
+                console.error('Error occurred');
+                return false;
+            }
+        }
+    };
+
+    const handleTaskDelete = (taskId: string | number): void => {
+        setTasks((prevTasks: Task[]): Task[] => {
+            return prevTasks.filter((task) => task.id !== taskId);
+        });
+    };
+
+    // On page load get all the tasks and add them to state
+    useEffect(() => {
+        axios.get('/api/tasks').then(({ data }) => {
+            setTasks(data);
+        });
+    }, []);
+
+    // For debugging
+    useEffect(() => {
+        console.log(tasks);
+    }, [tasks]);
+
+    return (
+        <>
+            <h1 className="mb-4 text-3xl font-bold text-center">Add a task</h1>
+
+            <div className="p-8">
+                <form onSubmit={handleSubmit} className="flex gap-2">
+                    <input
+                        type="text"
+                        className="flex-1 px-4 py-2 rounded bg-[#1a1a1a] focus:outline-none focus:ring-2 focus:ring-[#646cff] transition-all duration-200"
+                        placeholder="Task title"
+                        onInput={(e) => setTitle(e.currentTarget.value)}
+                        value={title}
+                    />
+                    <button
+                        type="submit"
+                        aria-label="Add task"
+                        className="text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
+                    >
+                        <IoMdAddCircleOutline title="Add task" />
+                    </button>
+                </form>
             </div>
-            <div className="flex items-center justify-between gap-2">
-              <div>
-                {taskBeingEdited?.id === task.id ? (
-                  <button
-                    aria-label="Save edited task"
-                    className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
-                    onClick={saveEditedTask}
-                  >
-                    <IoMdSave title="Save edited task" />
-                  </button>
-                ) : (
-                  <button
-                    aria-label="Edit title"
-                    className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
-                    onClick={() => task.id && handleEditTask(task.id)}
-                  >
-                    <AiOutlineEdit title="Edit title" />
-                  </button>
-                )}
-              </div>
-              {/* <div>
-                <button
-                  aria-label="Mark Complete"
-                  className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
-                  onClick={() => handleTaskComplete(task.id)}
-                >
-                  <AiOutlineFileDone title="Mark as complete" />
-                </button>
-              </div> */}
-              <div>
-                <button
-                  aria-label="Delete Task"
-                  className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-red-500"
-                  onClick={() => task.id && handleTaskDelete(task.id)}
-                >
-                  <AiOutlineDelete title="Delete task" />
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </ul>
-    </>
-  );
+
+            <ul className="mt-4 space-y-2">
+                {tasks.map((task) => (
+                    <div
+                        key={task.id}
+                        className="flex justify-between items-center p-4 rounded border border-[#646cff]"
+                    >
+                        <div>
+                            {taskBeingEdited?.id === task.id ? (
+                                <input
+                                    type="text"
+                                    className="flex-1 px-4 py-2 rounded bg-[#1a1a1a] focus:outline-none focus:ring-2 focus:ring-[#646cff] transition-all duration-200"
+                                    placeholder="Task title"
+                                    onInput={(e) => setEditedTitle(e.currentTarget.value)}
+                                    onBlur={saveEditedTask}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            saveEditedTask();
+                                        }
+                                    }}
+                                    value={editedTitle}
+                                />
+                            ) : (
+                                <span className="text-white">{task.title}</span>
+                            )}
+                        </div>
+                        <div className="flex items-center justify-between gap-2">
+                            <div>
+                                {taskBeingEdited?.id === task.id ? (
+                                    <button
+                                        aria-label="Save edited task"
+                                        className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
+                                        onClick={saveEditedTask}
+                                    >
+                                        <IoMdSave title="Save edited task" />
+                                    </button>
+                                ) : (
+                                    <button
+                                        aria-label="Edit title"
+                                        className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
+                                        onClick={() => task.id && handleEditTask(task.id)}
+                                    >
+                                        <AiOutlineEdit title="Edit title" />
+                                    </button>
+                                )}
+                            </div>
+                            {/* <div>
+								<button
+									aria-label="Mark Complete"
+									className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-green-500"
+									onClick={() => handleTaskComplete(task.id)}
+								>
+									<AiOutlineFileDone title="Mark as complete" />
+								</button>
+							</div> */}
+                            <div>
+                                <button
+                                    aria-label="Delete Task"
+                                    className="flex text-3xl text-white transition-colors duration-200 cursor-pointer hover:text-red-500"
+                                    onClick={() => task.id && handleTaskDelete(task.id)}
+                                >
+                                    <AiOutlineDelete title="Delete task" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default App;
